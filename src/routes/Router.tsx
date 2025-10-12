cba684d7f52169a8236ec7f7d037495e38f8ee71
Choi Yong Won <cywin0821@gmail.com>
2025-10-12 14:32:49 +0900

    chore: 린트 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "@pages/home/HomePage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from '@pages/home/HomePage';
 
 const Router = () => {
   return (
