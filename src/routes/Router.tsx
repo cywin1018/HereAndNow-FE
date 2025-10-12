daa38e91e9382c304c3928fc09fe541ceb6598f0
Choi Yong Won <cywin0821@gmail.com>
2025-10-12 13:50:57 +0900

    chore: tailwind 설정완료

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default Router;
