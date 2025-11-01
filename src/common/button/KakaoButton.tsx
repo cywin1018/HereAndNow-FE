2bfbd2a3d02de5fce6a88be4e59a1709c9e51325
Choi Yong Won <cywin0821@gmail.com>
2025-11-01 10:19:21 +0900

    chore: 로그인 페이지 제작

--- src/common/button/KakaoButton.tsx
+++ src/common/button/KakaoButton.tsx
@@ -1,10 +1,14 @@
 import kakaoIcon from '@assets/icons/kakaoIcon.svg';
 
-const KakaoButton = () => {
+interface KakaoButtonProps {
+  handleLogin: () => void;
+}
+
+const KakaoButton = ({ handleLogin }: KakaoButtonProps) => {
   return (
     <button
       className="text-s5 flex w-full items-center justify-between rounded-[8px] bg-[#FFEB00] px-4 py-3 text-black transition-colors hover:bg-yellow-400"
-      // onClick={handleLogin}
+      onClick={handleLogin}
     >
       <img src={kakaoIcon} alt="카카오 아이콘" className="h-5 w-5" />
 
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,16 +1,18 @@
 import GoogleButton from '@common/button/GoogleButton';
 import KakaoButton from '@common/button/KakaoButton';
+import useKakaoLogin from '@hooks/home/useKakaoLogin';
 
 const LoginPage = () => {
+  const { handleKakaoLogin } = useKakaoLogin();
   return (
-    <div className="flex h-screen flex-col justify-between p-6">
+    <div className="flex h-screen flex-col justify-between">
       <div className="flex flex-grow flex-col items-center justify-center">
         <img src="/LogoSection.png" alt="Here&Now 로고" className="w-48" />
         <p className="text-neutral-5 mt-4">발견한 곳이 곧 우리의 이야기가 되다</p>
       </div>
 
       <div className="flex flex-col gap-y-3">
-        <KakaoButton />
+        <KakaoButton handleLogin={handleKakaoLogin} />
         <GoogleButton />
       </div>
     </div>
