581f8bde45e96e2abff062eb3661ba53376f57a3
Choi Yong Won <cywin0821@gmail.com>
2025-11-01 10:39:22 +0900

    feat: 공용 헤더파일 적용

--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 interface LayoutProps {
   withHeader?: boolean;
@@ -8,11 +9,7 @@ const Layout = ({ withHeader = true }: LayoutProps) => {
   return (
     <div className="min-h-screen">
       <main className="mx-auto min-h-screen w-full max-w-md border bg-white">
-        {withHeader && (
-          <header className="border-b p-4">
-            <h1 className="text-s1 text-neutral-13 text-center">임시 헤더</h1>
-          </header>
-        )}
+        {withHeader && <Header />}
 
         <section className="p-4">
           <Outlet />
