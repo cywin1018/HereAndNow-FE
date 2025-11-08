eb2be11b5fa7ed46d3ca0ebcc5e36eef90a81003
박진우 <nozick1021@gmail.com>
2025-11-08 16:44:00 +0900

    feat: 별점 필터링 컴포넌트화

--- src/pages/archive/ArchiveSearchPage.tsx
+++ src/pages/archive/ArchiveSearchPage.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+import StarRatingFilter from '@common/components/StarRatingFilter';
+
 const ArchiveSearchPage = () => {
+  const [rating, setRating] = useState<number>(0);
+
   return (
-    <div>
-      <h1>ArchiveSearchPage</h1>
+    <div className="bg-neutral-1 flex min-h-screen flex-col px-5">
+      <span className="text-h4 text-neutral-10 py-8">찾고 싶은 추억이 있나요?</span>
+
+      {/* 검색 필터 리스트 */}
+      <div className="flex w-full flex-col gap-8">
+        <StarRatingFilter rating={rating} onRatingChange={setRating} title="별점으로 찾아볼까요?" />
+      </div>
     </div>
   );
 };
