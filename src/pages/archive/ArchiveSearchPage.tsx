e4994634732071c97c13d0aa871cdbf68dee164f
박진우 <nozick1021@gmail.com>
2025-11-08 16:55:33 +0900

    feat: 전체 section에 px-5 하도록 수정

--- src/pages/archive/ArchiveSearchPage.tsx
+++ src/pages/archive/ArchiveSearchPage.tsx
@@ -5,7 +5,7 @@ const ArchiveSearchPage = () => {
   const [rating, setRating] = useState<number>(0);
 
   return (
-    <div className="bg-neutral-1 flex min-h-screen flex-col px-5">
+    <div className="bg-neutral-1 flex min-h-screen flex-col">
       <span className="text-h4 text-neutral-10 py-8">찾고 싶은 추억이 있나요?</span>
 
       {/* 검색 필터 리스트 */}
