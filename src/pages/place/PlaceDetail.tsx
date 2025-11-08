5958944052d95f7f8129afc286bc121e5fa38ef6
Choi Yong Won <cywin0821@gmail.com>
2025-11-08 17:36:53 +0900

    feat: 장소 상세 페이지 컴포넌트 추가

---
+++ src/pages/place/PlaceDetail.tsx
@@ -0,0 +1,11 @@
+import PageHeader from '@common/layout/PageHeader';
+
+const PlaceDetail = () => {
+  return (
+    <div className="flex w-full flex-col">
+      <PageHeader title="장소 상세" />
+    </div>
+  );
+};
+
+export default PlaceDetail;
