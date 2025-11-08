bd99280bf70a324e7ffc42df2339be70daf28058
박진우 <nozick1021@gmail.com>
2025-11-08 17:30:42 +0900

    feat: 전역 모달 컴포넌트 생성

--- src/pages/archive/ArchiveSearchPage.tsx
+++ src/pages/archive/ArchiveSearchPage.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import StarRatingFilter from '@common/components/StarRatingFilter';
+import Modal from '@common/components/Modal';
 
 const ArchiveSearchPage = () => {
   const [rating, setRating] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <div className="bg-neutral-1 flex min-h-screen flex-col">
@@ -12,6 +14,17 @@ const ArchiveSearchPage = () => {
       <div className="flex w-full flex-col gap-8">
         <StarRatingFilter rating={rating} onRatingChange={setRating} title="별점으로 찾아볼까요?" />
       </div>
+
+      <button onClick={() => setIsOpen(true)}>연인을 초대해주세요</button>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        mainMessage="연인을 초대해주세요"
+        subMessage="커넥팅 화면에서 연인을 초대 후 이용 가능해요"
+        leftButtonText="홈으로"
+        rightButtonText="연인 초대하기"
+      />
     </div>
   );
 };
