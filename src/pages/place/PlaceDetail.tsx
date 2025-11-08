bd3b7d366877b058aa5b370094847f8e0bf62c4c
Choi Yong Won <cywin0821@gmail.com>
2025-11-08 17:37:02 +0900

    fix: 업데이트

--- src/pages/place/PlaceDetail.tsx
+++ src/pages/place/PlaceDetail.tsx
@@ -3,7 +3,7 @@ import PageHeader from '@common/layout/PageHeader';
 const PlaceDetail = () => {
   return (
     <div className="flex w-full flex-col">
-      <PageHeader title="장소 상세" />
+      <PageHeader title="장소 세부설명" />
     </div>
   );
 };
