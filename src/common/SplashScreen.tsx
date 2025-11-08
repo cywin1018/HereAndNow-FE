2fc868f71ccac8dfd950ded35707ddf8bc8d87b9
Choi Yong Won <cywin0821@gmail.com>
2025-11-08 13:23:31 +0900

    feat: 로딩컴포넌트 제작 및 스플래시 이미지 수정

--- src/common/SplashScreen.tsx
+++ src/common/SplashScreen.tsx
@@ -1,7 +1,9 @@
 const SplashScreen = () => {
   return (
-    <div className="fixed top-0 left-0 z-[9999] flex h-screen w-screen items-center justify-center bg-white">
-      <img src="/splashLogo.svg" alt="Loading..." className="w-20" />
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-white">
+      <div className="w-full max-w-[402px]">
+        <img src="/hereandnow-splash.gif" alt="Here and Now 로딩 중" className="w-full" />
+      </div>
     </div>
   );
 };
