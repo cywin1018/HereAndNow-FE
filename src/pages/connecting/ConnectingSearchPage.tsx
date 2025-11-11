d1979ceb13361b6a013004a647f082265e6f7b01
Choi Yong Won <cywin0821@gmail.com>
2025-11-11 22:21:15 +0900

    feat: 커넥팅 검색 페이지 초안

---
+++ src/pages/connecting/ConnectingSearchPage.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import StarRatingFilter from '@common/components/StarRatingFilter';
+
+const ConnectingSearchPage = () => {
+  const [rating, setRating] = useState<number>(0);
+
+  return (
+    <div className="bg-neutral-1 flex min-h-screen flex-col">
+      <span className="text-h4 text-neutral-10 py-8">찾고 싶은 추억이 있나요?</span>
+
+      <div className="flex w-full flex-col gap-8">
+        <StarRatingFilter rating={rating} onRatingChange={setRating} title="별점으로 찾아볼까요?" />
+      </div>
+    </div>
+  );
+};
+
+export default ConnectingSearchPage;
