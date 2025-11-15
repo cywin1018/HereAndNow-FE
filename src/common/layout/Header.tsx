e21f3e3d5ed4b5735ab45515bdbd4297253be7f0
Choi Yong Won <cywin0821@gmail.com>
2025-11-15 13:37:35 +0900

    feat: 알람모달 ui 생성

--- src/common/layout/Header.tsx
+++ src/common/layout/Header.tsx
@@ -1,22 +1,33 @@
+import { useState } from 'react';
 import AlertIcon from '@assets/icons/alert.svg';
 import MypageICon from '@assets/icons/mypage.svg';
+import { AlarmModal, dummyAlarms } from '@common/alarm';
 
 const Header = () => {
-  return (
-    <header className="p-4">
-      <div className="flex items-center justify-between">
-        <img src="/LogoIcon.svg" alt="Here&Now 로고" className="h-7 w-auto" />
+  const [isAlarmOpen, setIsAlarmOpen] = useState(false);
 
-        <div className="flex items-center gap-x-[8px]">
-          <button type="button" className="p-2">
-            <img src={AlertIcon} alt="알림" className="h-[28px] w-[28px]" />
-          </button>
-          <button type="button" className="p-2">
-            <img src={MypageICon} alt="마이페이지" className="h-[28px] w-[28px]" />
-          </button>
+  const handleAlarmClick = () => {
+    setIsAlarmOpen(true);
+  };
+
+  return (
+    <>
+      <header className="p-4">
+        <div className="flex items-center justify-between">
+          <img src="/LogoIcon.svg" alt="Here&Now 로고" className="h-7 w-auto" />
+
+          <div className="flex items-center gap-x-[8px]">
+            <button type="button" className="p-2" onClick={handleAlarmClick}>
+              <img src={AlertIcon} alt="알림" className="h-[28px] w-[28px]" />
+            </button>
+            <button type="button" className="p-2">
+              <img src={MypageICon} alt="마이페이지" className="h-[28px] w-[28px]" />
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <AlarmModal isOpen={isAlarmOpen} onClose={() => setIsAlarmOpen(false)} alarms={dummyAlarms} />
+    </>
   );
 };
 
