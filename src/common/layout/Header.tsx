7b48ff4803325cdf94bf38f7617e5b10eff14628
Choi Yong Won <cywin0821@gmail.com>
2025-11-15 13:54:20 +0900

    feat: 마이페이지 ui

--- src/common/layout/Header.tsx
+++ src/common/layout/Header.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import AlertIcon from '@assets/icons/alert.svg';
 import MypageICon from '@assets/icons/mypage.svg';
 import { AlarmModal, dummyAlarms } from '@common/alarm';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const [isAlarmOpen, setIsAlarmOpen] = useState(false);
-
+  const navigate = useNavigate();
   const handleAlarmClick = () => {
     setIsAlarmOpen(true);
+  };
+
+  const handleMypageNavigation = () => {
+    navigate('/mypage');
   };
 
   return (
@@ -20,7 +25,7 @@ const Header = () => {
             <button type="button" className="p-2" onClick={handleAlarmClick}>
               <img src={AlertIcon} alt="알림" className="h-[28px] w-[28px]" />
             </button>
-            <button type="button" className="p-2">
+            <button type="button" className="p-2" onClick={handleMypageNavigation}>
               <img src={MypageICon} alt="마이페이지" className="h-[28px] w-[28px]" />
             </button>
           </div>
